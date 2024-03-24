3902d44c820d7b17fa7c1ddaaebcc72e847a6e8d
Lior Atar <Liorat18@gmail.com>
2024-03-24 23:32:22 +0200

    section 8

---
+++ section8/oop_homework.py
@@ -0,0 +1,57 @@
+# Problem 1
+# Fill in the Line class methods to accept coordinates as a pair of tuples
+# and return the slope and distance of the line.
+
+import math
+
+class Line:
+
+    def __init__(self, coor1, coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+
+    def distance(self):
+        return math.sqrt((self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2)
+
+    def slope(self):
+        return (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0])
+
+
+# EXAMPLE OUTPUT
+
+coordinate1 = (3, 2)
+coordinate2 = (8, 10)
+
+li = Line(coordinate1,coordinate2)
+
+print('distance', li.distance())
+# 9.433981132056603
+
+print('slope', li.slope())
+# 1.6
+
+
+# Problem 2
+# Fill in the class
+
+class Cylinder:
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        return math.pi*(self.radius**2)*self.height
+
+    def surface_area(self):
+        return 2*math.pi*(self.radius**2) + 2*math.pi*self.radius*self.height
+
+
+# EXAMPLE OUTPUT
+c = Cylinder(2, 3)
+
+print('volume', c.volume())
+# 56.52
+
+print('surface area', c.surface_area())
+# 94.2`
